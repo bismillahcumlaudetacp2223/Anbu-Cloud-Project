bde647e4668da9f136562ef7076c0d3d078194db
nadiawaa <wogjowosaklawase64@gmail.com>
2023-07-08 21:09:05 +0700

    add update terraform script

---
+++ anbu-devops-central/anbu-devops-cluster-firewall/variables.tf
@@ -0,0 +1,8 @@
+# -----------------------------------------------------------------------------
+# VARIABLE
+# -----------------------------------------------------------------------------
+variable project_id {
+  type        = string
+  description = "The project ID to host the resources in"
+  default = "anbu-devops"
+}
