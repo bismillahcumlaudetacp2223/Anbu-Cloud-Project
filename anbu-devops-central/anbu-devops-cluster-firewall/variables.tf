4f2bc464b5a3da5526c89bb56542e617411c4bf0
pandudw <nasrullah.pandud1@gmail.com>
2023-07-08 21:18:29 +0700

    remove unused terraform script

--- anbu-devops-central/anbu-devops-cluster-firewall/variables.tf
+++
@@ -1,8 +0,0 @@
-# -----------------------------------------------------------------------------
-# VARIABLE
-# -----------------------------------------------------------------------------
-variable project_id {
-  type        = string
-  description = "The project ID to host the resources in"
-  default = "anbu-devops"
-}
