518559a16d9d9f4f9800908164edd9aba31ad644
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2023-07-09 07:08:37 +0700

    02_Infraswitch on off (#2)
    * add initial script to on/off infra automatically
    
    * update script infraswitch
    
    ---------
    
    Co-authored-by: pandudw <nasrullah.pandud1@gmail.com>

---
+++ infraswitch/scaledown.sh
@@ -0,0 +1,19 @@
+# Scale Down GKE Node
+echo 'Starting Scale Down GKE Node Process ...'
+gcloud container clusters get-credentials cc-project-capstone --region us-central1 --project cc-capstone-ta
+gcloud container clusters resize cc-project-capstone --num-nodes=0 --zone=us-central1
+echo 'Scale Down GKE Node Process, Done!'
+
+# Patch PDB to 0
+echo 'Starting Patch PDB to 0 ...'
+for pdb in $(kubectl get pdb -n default -o=name); do
+  kubectl patch "$pdb" -n default> --type='json' -p='[{"op": "replace", "path": "/spec/minAvailable", "value": 0}]'
+done
+echo 'Patch PDB to 0, Done!'
+
+echo 'Starting Scale Down Replicas to 0 ...'
+# Scale Replicas to 0
+for deployment in $(kubectl get deployments -n default -o=name); do
+  kubectl scale "$deployment" --replicas=<0 -n default
+done
+echo 'Scale Down Replicas, Done!'
